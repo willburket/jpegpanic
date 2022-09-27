f52de9df297e1025ac99c6d32038c712a11abb1c
Will Burket <willburket119@gmail.com>
2022-09-27 19:49:45 -0400

    add spinner while loading articles

--- src/static/main.js
+++ src/static/main.js
@@ -2,36 +2,39 @@
 let visible = 3
 
 const articlesBox = document.getElementById('articles-box')
-const spinnerBox = document.getElementById('spinner-box')
-
-
-const handleGetData = () => {
-    $.ajax({
-        type: 'GET',
-        url: `/articles-json/${visible}`,
-        success: function(response){
-            //console.log(response.max)
-            maximum_size = response.max
-            const data = response.data
-            data.map(post=>{
-                console.log(post.id)
-            })
-            if(maximum_size){
-                console.log('Done')
-            }
-        },
-        error: function(error){
-            console.log(error)
-        }
-    })
-
-}
-
-handleGetData()
-
 
 window.onload = function(){
     const loadBtn = document.getElementById('load-btn')
+    const spinnerBox = document.getElementById('spinner-box')
+    
+    const handleGetData = () => {
+        $.ajax({
+            type: 'GET',
+            url: `/articles-json/${visible}`,
+            success: function(response){
+                //console.log(response.max)
+                maximum_size = response.max
+                const data = response.data
+                spinnerBox.classList.remove('not-visible')
+                setTimeout(()=>{
+                    spinnerBox.classList.add('not-visible')
+                    data.map(post=>{
+                        console.log(post.id)
+                    })
+                ,500})
+                
+                if(maximum_size){
+                    console.log('Done')
+                }
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    
+    }
+
+    handleGetData()
     loadBtn.addEventListener('click', ()=>{
         visible += 3
         handleGetData()
